74baa83d5db71c22eced6779b7f62984b10da38d
Yura Samkov <y.samkoff@ukr.net>
2016-01-30 23:06:28 +0200

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])   # або  @user = User.find(1)
+  end 
+
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!" # Добавление флеш сообщения
+                                                    # к успешной регистрации пользователя. 
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-
-  get 'users/new'
+  
+  resources :users
+  #get 'users/new'
 
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
